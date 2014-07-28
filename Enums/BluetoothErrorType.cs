8f49b51d6b4436b1f90a9b131904bbc4b2f11790
Stanilav Hacker <info@dbsys.cz>
2014-07-28 22:49:46 +0200

    Utils

---
+++ Enums/BluetoothErrorType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoPilot.Utils.Enums
+{
+    public enum BluetoothErrorType
+    {
+        None,
+        NotEnabled,
+        NotFound,
+        NoDevice,
+        OutOfRange,
+        NotSelected,
+        NotAllowed,
+        FatalError,
+        Unknown
+    }
+}
